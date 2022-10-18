ac3d8ce9be109229c46bf035d444db7dd5edf6b3
rameshalagumalai <rameshalagumalai@github.com>
2022-10-18 20:58:09 +0530

    add project solution fit

--- project development phase/sprint 1/expense_tracker/index.py
+++ project development phase/sprint 1/expense_tracker/index.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/sign-up")
 def hello_world():
     return render_template('register.html')
+
+@app.route("/sign-in")
+def toSignUpPage():
+    return render_template('signin.html')
