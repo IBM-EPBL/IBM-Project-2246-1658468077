04cdd262f6e5f4028178816d0160789d51c72542
rameshalagumalai <rameshalagumalai@github.com>
2022-10-20 13:27:57 +0530

    add technology stack

--- project development phase/sprint 1/expense_tracker/index.py
+++ project development phase/sprint 1/expense_tracker/index.py
@@ -1,6 +1,34 @@
 from flask import Flask, render_template
+import ibm_db;
 
 app = Flask(__name__)
+
+dsn_hostname = "3883e7e4-18f5-4afe-be8c-fa31c41761d2.bs2io90l08kqb1od8lcg.databases.appdomain.cloud"
+dsn_uid = "ghc72736"
+dsn_pwd = "uz905NueevL1upTM"
+
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_database = "BLUDB"
+dsn_port = "50000"
+dsn_protocol = "TCPIP"
+
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+).format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd)
+
+print(dsn)
+
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print("Connected to database: ", dsn_database, " as user: ", dsn_uid, " on host: ", dsn_hostname)
+except:
+    print("Unable to connect: ", ibm_db.conn_errormsg())
 
 @app.route("/sign-up")
 def hello_world():
@@ -12,4 +40,6 @@ def toSignUpPage():
 
 @app.route("/")
 def toHomePage():
-    return render_template('home.html')    
+    return render_template('home.html')  
+
+
